efaa4b94fdc0071ea068b78d56be36a62a261df9
Jakub <dzejkobjj@gmail.com>
2024-03-09 23:58:06 +0100

    clearing interval when triggered refreshing

--- content-script.js
+++ content-script.js
@@ -1,10 +1,18 @@
 window.onload = (event) => {
-    setInterval(() => {
+    let stopRefreshing = false
+    var intervalId = setInterval(() => {
         let bannerId = document.getElementById('bannerId')
         var errorText =
             bannerId?.childNodes[0]?.childNodes[1]?.childNodes[0].textContent
         if (errorText == 'Please try again or select a different event') {
+            stopRefreshing = true
             location.reload()
         }
     }, 1000)
+
+    setInterval(() => {
+        if (stopRefreshing) {
+            clearInterval(intervalId)
+        }
+    }, 300)
 }
